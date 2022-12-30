046344aa0aed3ce553e473eddd48eab1ff792ffc
Ben DaGloria Rifleman <102486084+rifleben@users.noreply.github.com>
2022-12-29 19:38:58 -0800

    Update sms_send.py

--- sms_send.py
+++ sms_send.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 
 
 def send_sms(message, phone):
-    """Send SMS message to user."""
+    """Send SMS message to user with message."""
     account_sid = "YOUR_ACCOUNT_SID"
     auth_token = "YOUR_AUTH_TOKEN"
     client = Client(account_sid, auth_token)
